5b8f09a405bad418bc9af55d79afa8e9913459cf
Leonard Odoi <odoi.leonard1@gmail.com>
2023-04-11 22:54:05 -0700

    Refactor code

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,11 +4,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = 
+        self.__size = size 
