d135cafef6ecb1036342e852f90e3108f5b2ec4e
Leonard Odoi <odoi.leonard1@gmail.com>
2023-06-05 16:34:49 -0700

    Include docstring of function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+
+This module is comprises a function that adds two numbers
+
+"""
+
+def add_integer(a, b=98):
+        """This function adds two integers and/or float numbers
+
+    Parameters:
+        a: first number
+        b: second number
+
+    Returns:
+        The addition of the two given numbers
+
+    Raises:
+        TypeError: If a or b are not integers and/or float numbers
+
+    """
