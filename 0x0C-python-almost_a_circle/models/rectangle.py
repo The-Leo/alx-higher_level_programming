fc3da26618a9d7e53741792e57845cfe82075770
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-17 16:39:47 -0700

    Add getter and setter function for height

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -42,3 +42,25 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
 
         self.__width = value
+
+            @property
+    def height(self):
+        """
+            getter function for height
+            Returns: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            setter function for height
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
