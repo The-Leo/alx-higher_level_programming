44e38efea7f870b0fa3f2c8c30a8b0dc8e476e55
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-17 16:38:18 -0700

    Add getter and setter function for width

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,3 +20,25 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+        @property
+    def width(self):
+        """
+            getter function for __width
+            Returns: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            setter function for width.
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
