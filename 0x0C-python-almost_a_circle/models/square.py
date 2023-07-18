54cfd678c512e689d6d07559919ca8e72b96415e
Leonard Odoi <odoi.leonard1@gmail.com>
2023-07-17 17:30:41 -0700

    Update the class Square by adding the public getter and setter size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,23 @@ class Square(Rectangle):
             initialises Square (overrides Rectangle init)
         """
         super().__init__(size, size, x, y, id)
+
+            @property
+    def size(self):
+        """
+            returns the size of the square
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+            sets the value of size
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
